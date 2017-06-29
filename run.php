c96fd48f99a99db61b33433d2462e55177344380
Eric Stern <eric@ericstern.com>
2017-06-29 12:14:49 -0700

    Document the run process

--- run.php
+++ run.php
@@ -15,20 +15,28 @@ if (isset($opts['v'])) {
 }
 
 $logger = new Logger($level);
+
+// Open subprocess which starts the language server. We will communicate it via
+// STDIN and STDOUT
 $proc = new Process($_SERVER['_'] . ' vendor/bin/php-language-server.php', $logger);
 
+// Format and send the initialize message
 $init = Message\Request::factory('initialize', [
     'rootPath' => getcwd(),
     'processId' => getmypid(),
     'capabilities' => (object)[],
 ]);
-
 $proc->write($init);
 
-$proc->onRead(function(Message\Message $message) use ($logger) {
+// Register a callback to handle inbound messages. In a more useful context,
+// you'd do something more than log them. Possibly respond with a new one
+$proc->onRead(function (Message\Message $message) use ($logger, $proc) {
     if ($message->getMethod() !== 'window/logMessage') {
         $logger->info($message);
     }
+    // Send a message back? $proc->write($formattedMessage);
 });
 
+// Infinite loop, reading out of the subprocess's STDOUT for messages that it
+// sends. Any time one is received, it will fire the above callback.
 $proc->listen();
